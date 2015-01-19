7076915b9fc50e94987ee42fab322d309adcbf8b
Avi Dunn <avidunn@gmail.com>
2015-01-18 17:18:51 -0800

    Refactored taxonomy

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,20 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function() {
+  var str = 'I am too old, let me play cards instead';
+  return str;
+};
+
+RetiredForagerBee.prototype.gamble = function() {
+  this.treasureChest.push('Huzah');
+};
